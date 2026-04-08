bc8d02d082453573770205e2405d5b92151841cd
HafeezaBegum <hafeeza99gm@gmail.com>
2026-04-08 10:04:16 -0400

    Updated resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,55 +2,55 @@ import React from "react";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const NAVBAR_HEIGHT_OFFSET = -50; 
+    const NAVBAR_HEIGHT_OFFSET = -50;
 
-  return (
-    <nav className="navbar navbar-expand-lg fixed-top navbar-custom-grey">
-      <div className="container">
-        <Link to="home" smooth duration={300} className="navbar-brand btn btn-link" offset={NAVBAR_HEIGHT_OFFSET}> 
-            My Portfolio
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link activeClass="active" to="home" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Home</Link> 
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="education" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Education</Link>
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="skills" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Skills</Link> 
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="projects" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Projects</Link> 
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="experience" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Experience</Link> 
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="certifications" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Certifications</Link> 
-            </li>
-            <li className="nav-item">
-                <a
-                    href="https://drive.google.com/file/d/1S9eUkB5Y_CpDxmZpEfWgBaAIxooLw3Cr/view?usp=sharing"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="nav-link"
-                >
-                    Resume
-                </a>
-            </li>
-            <li className="nav-item">
-                <Link activeClass="active" to="contact" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Contact</Link> 
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top navbar-custom-grey">
+            <div className="container">
+                <Link to="home" smooth duration={300} className="navbar-brand btn btn-link" offset={NAVBAR_HEIGHT_OFFSET}>
+                    My Portfolio
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link activeClass="active" to="home" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="education" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Education</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="skills" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Skills</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="projects" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Projects</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="experience" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Experience</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="certifications" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Certifications</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                href="https://drive.google.com/file/d/1glqWO_WBXkNSGo1AAjVS9aFSfTVqcNyg/view?usp=sharing"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="nav-link"
+                            >
+                                Resume
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <Link activeClass="active" to="contact" smooth duration={300} className="nav-link" offset={NAVBAR_HEIGHT_OFFSET}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
